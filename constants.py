ea350a55a6e271aa09ca6f8bbdb0ef0a7f6b8eb4
Naeem Iqbal <naeem.iqbal@dfki.de>
2021-01-15 11:42:02 +0100

    active learning files

---
+++ constants.py
@@ -0,0 +1,14 @@
+import os
+
+# HDD_DATASET_ROOT = "./dataset"
+# SSD_DATASET_ROOT = "./dataset"
+HDD_DATASET_ROOT = "/home/robot/datasets/structured_cwc/"
+SSD_DATASET_ROOT = "/home/robot/datasets/structured_cwc/"
+RUNS = "./runs"
+DEPTH_WIDTH = 320
+DEPTH_HEIGHT = 240
+MC_STEPS = 20
+MC_DROPOUT_RATE = 0.25
+WORLD_DISTANCE_THRESHOLD = 1.0
+COVERAGE_IGNORE_THRESHOLD = 0.35
+VISUALIZATION = True
